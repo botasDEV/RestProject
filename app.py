037ff45d262c75277d2f1a2647f06cb07e06dd55
Rafael Botas <rafaelalexandre.botasrosado@gmail.com>
2019-07-08 22:52:01 +0100

    Set port

--- app.py
+++ app.py
@@ -43,4 +43,5 @@ def check_if_token_in_blacklist(token):
 
 
 if __name__ == '__main__':
-    app.run(port=8000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
